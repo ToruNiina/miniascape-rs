eeec0ec66a441485f415074985f9c7ff3a05b29c
ToruNiina <niina.toru.68u@gmail.com>
2022-08-12 22:39:39 +0900

    doc: add link to repo

--- src/wrap_app.rs
+++ src/wrap_app.rs
@@ -404,7 +404,9 @@ impl eframe::App for WrapApp {
             ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
                 ui.horizontal(|ui| {
                     ui.spacing_mut().item_spacing.x = 0.0;
-                    ui.label("powered by ");
+                    ui.label("source code: ");
+                    ui.hyperlink_to("miniascape", "https://github.com/ToruNiina/miniascape-rs");
+                    ui.label(", powered by ");
                     ui.hyperlink_to("egui", "https://github.com/emilk/egui");
                     ui.label(" and ");
                     ui.hyperlink_to("eframe", "https://github.com/emilk/egui/tree/master/eframe");
